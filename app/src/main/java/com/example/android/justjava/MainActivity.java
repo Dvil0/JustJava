c29216f2d540517ba09cfee72d5d7000f75077e3
Dvil0 <Dvil0@users.noreply.github.com>
2019-04-01 03:07:38 -0500

    [Dv] - complete the app with increment and decrement into layout.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quiantity = 2;
-        display(quiantity);
-        displayPrice(quiantity* 5);
+        //display(quantity);
+        //displayPrice(quantity* 5);
+        String priceMessage = "Price $" + (quantity * 5);
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -60,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void increment(View view){
-        int quiantity = 3;
-        display(quiantity);
+        quantity = quantity + 1;
+        display(quantity);
     }
 
     /**
@@ -69,7 +73,15 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void decrement(View view){
-        int quiantity = 1;
-        display(quiantity);
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
